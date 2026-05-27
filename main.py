b30cf66691f7e58f6a5a0f5799e8a0216748470f
PARNIKA <moolaparnika.15@gmail.com>
2026-05-27 13:52:02 +0530

    Added product search feature

--- main.py
+++ main.py
@@ -98,6 +98,32 @@ def view_cart(user_id):
         total += price*quantity
         
     print("Total cart amount:",total)
+    
+# ---------------- SEARCH PRODUCT ----------------    
+    
+def search_product():
+
+    keyword = input("Enter Product Name To Search: ")
+
+    query = """
+    SELECT * FROM products
+    WHERE product_name LIKE %s
+    """
+
+    cursor.execute(query, ("%" + keyword + "%",))
+
+    products = cursor.fetchall()
+
+    if products:
+
+        print("\nSearch Results\n")
+
+        for p in products:
+            print("ID:", p[0], "Name:", p[1],
+                  "Price:", p[2], "Stock:", p[3])
+
+    else:
+        print("No Products Found")
 
 
 # ---------------- PLACE ORDER ----------------
@@ -244,28 +270,32 @@ while True:
 
                 print("\n----- USER MENU -----")
                 print("1 View Products")
-                print("2 Add To Cart")
-                print("3 View Cart")
-                print("4 Place Order")
-                print("5 Payment")
-                print("6 Order History")
-                print("7 Logout")
+                print("2 Search Product")
+                print("3 Add To Cart")
+                print("4 View Cart")
+                print("5 Place Order")
+                print("6 Payment")
+                print("7 Order History")
+                print("8 Logout")
 
                 ch = input("Enter Choice: ")
 
                 if ch == "1":
                     view_products()
-
+                    
                 elif ch == "2":
-                    add_to_cart(user_id)
+                    search_product()
 
                 elif ch == "3":
-                    view_cart(user_id)
+                    add_to_cart(user_id)
 
                 elif ch == "4":
-                    total = place_order(user_id)
+                    view_cart(user_id)
 
                 elif ch == "5":
+                    total = place_order(user_id)
+
+                elif ch == "6":
                 #     make_payment(user_id,total)
                 #     move_to_history(user_id)elif ch == "5":
                    if total > 0:
@@ -276,10 +306,10 @@ while True:
                    else:
                       print("No order placed yet")
 
-                elif ch == "6":
+                elif ch == "7":
                     view_order_history(user_id)
 
-                elif ch == "7":
+                elif ch == "8":
                     print("Thank You For Shopping")
                     break
 
